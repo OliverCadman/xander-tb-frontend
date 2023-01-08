982aee79fdb688c6cfaf7cc380a9317f825d6a21
Oliver Cadman <o.cadman@live.co.uk>
2023-01-08 21:53:47 +0000

    Update BASE_API_URL

--- assets/js/get_data.js
+++ assets/js/get_data.js
@@ -1,4 +1,4 @@
-BASE_API_URL = 'http://0.0.0.0:8000/api/orders'
+BASE_API_URL = "http://ec2-18-216-75-132.us-east-2.compute.amazonaws.com/api/orders";
 
 async function getData(endpoint) {
   const url = `${BASE_API_URL}${endpoint}`;
